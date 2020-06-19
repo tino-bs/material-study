9b20eba763a6268c99114b8f9e1b488171e89a80
tino.goetz <tino.goetz@mdm.de>
2020-06-19 12:29:24 +0200

    chore(ci): prepare for github pages deployment

--- study/.vuepress/config.js
+++ study/.vuepress/config.js
@@ -1,4 +1,4 @@
-const { description, repository } = require('../../package.json')
+const { description } = require('../../package.json')
 
 module.exports = {
   title: 'MDM / Design-Studie',
@@ -8,6 +8,7 @@ module.exports = {
   ],
   patterns: ['**/*.md', '!**/node_modules'],
   plugins: ['vuepress-plugin-medium-zoom'],
+  base: '/material-study/',
   themeConfig: {
     // GitHub-Repo is default
     repo: 'tino-bs/material-study',
